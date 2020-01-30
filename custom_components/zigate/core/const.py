37193f59a08ae66192274451b2be8d150f2ae60a
cyr-ius <cyr-ius@ipocus.net>
2020-01-30 07:57:09 +0100

    Update config_flow syntax

--- custom_components/zigate/core/const.py
+++ custom_components/zigate/core/const.py
@@ -2,6 +2,8 @@ DOMAIN = 'zigate'
 SCAN_INTERVAL = 120
 DATA_ZIGATE_DEVICES = 'zigate_devices'
 DATA_ZIGATE_ATTRS = 'zigate_attributes'
+DATA_ZIGATE_CONFIG = 'zigate_config'
+ZIGATE_ID = 'zigate_id'
 ADDR = 'addr'
 IEEE = 'ieee'
 SUPPORTED_PLATFORMS = (
@@ -13,4 +15,4 @@ SUPPORTED_PLATFORMS = (
     'climate'
 )
 PERSISTENT_FILE = 'zigate.json'
-AVAILABLE_MODES = ['usb','wifi','gpio']
+AVAILABLE_MODES = ['usb', 'wifi', 'gpio']
